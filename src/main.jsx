3c611aaace06ac4d0a3c324ce96195f7b32b5ea6
Your Name <traveligo00@gmail.com>
2025-07-29 11:28:35 +0530

    Remove Tailwind CSS and update configurations

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // Make sure this path is correct
+import App from './App' 
 import './index.css'
-// src/main.jsx
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
